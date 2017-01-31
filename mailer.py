db2df0410ddf6055101a614ad6b5d46cdd169fa3
Martin Chorley <me@martinjc.com>
2017-01-31 18:32:01 +0000

    I basically improved this 100,000%
    Mailer now initialised with server setup (that doesn't change) and send() receives the stuff that does change (message contents). Also can now send files as attachments

--- mailer.py
+++ mailer.py
@@ -1,18 +1,46 @@
+from email.mime.application import MIMEApplication
+from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
+from email.utils import COMMASPACE, formatdate
+from email import encoders
 import smtplib
-import config
+import os
 
 class MeetMail:
     """meet"""
 
-    def __init__(self, body, subject):
-        self.msg = MIMEText(body)
-        self.subject = subject
+    def __init__(self, uname, pwd, server, port):
 
-    def send(self, uname, pwd, recs):
+        self.msg = MIMEMultipart()
+        self.uname = uname
+        self.pwd = pwd
+        self.sender = smtplib.SMTP(server, port)
+
+
+
+    def send(self, send_from, send_to, subject, body, files=[]):
+
+        # header
+        self.msg['From'] = send_from
+        self.msg['To'] = COMMASPACE.join(send_to)
+        self.msg['Date'] = formatdate(localtime=True)
+        self.msg['Subject'] = subject
+
+        self.msg.attach(MIMEText(body))
+
+        # attach files
+        for f in files:
+            with open(f, 'rb') as attach_file:
+                part = MIMEApplication(
+                    attach_file.read(),
+                    Name = os.path.basename(f)
+                )
+                part['Content-Disposition'] = 'attachment; filename=%s' % os.path.basename(f)
+                self.msg.attach(part)
+
         """authenticate with SMTP server and send email"""
-        smtp_sender = smtplib.SMTP(config.smtp_server, config.smtp_port)
-        smtp_sender.ehlo()
-        smtp_sender.starttls()
-        smtp_sender.login(uname, pwd)
-        smtp_sender.sendmail(uname, recs, self.msg.as_string())
+        self.sender.ehlo()
+        self.sender.starttls()
+        self.sender.login(self.uname, self.pwd)
+        self.sender.sendmail(send_from, send_to, self.msg.as_string())
+        self.sender.quit()
